93548372b73309927f8bee4d32a8cfb1cf9fe9e5
Raimon López <raimonlpez@gmail.com>
2020-10-10 19:11:50 +0200

    Update server.ts

--- classes/server.ts
+++ classes/server.ts
@@ -2,11 +2,11 @@ import express from 'express';
 
 export default class Server {
     public app : express.Application;
-
+    
     //leer localhost de variables y puertp
-    host = process.env.host || '0.0.0.0';
+    host = process.env.HOST || '0.0.0.0';
     // port = process.env.PORT || 3000;
-    port = process.env.port || 3000;
+    port = process.env.PORT || 3000;
     //public port: number = 3000;
     
     constructor() {
